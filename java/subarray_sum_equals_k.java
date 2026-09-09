826387c92f2bd646b8f8da8c1fd5e077e0e8a150
Aditi <aditi1033.be24@chitkarauniversity.edu.in>
2026-09-09 14:05:52 +0530

    LeetCode: update java/subarray_sum_equals_k.java

---
+++ java/subarray_sum_equals_k.java
@@ -0,0 +1,29 @@
+// ======================================
+// LeetCode Problem: subarray sum equals k
+// Language: java
+// Link: https://leetcode.com/problems/subarray-sum-equals-k/
+// Synced by: LinkCode
+// Date: 9/9/2026, 2:05:51 pm
+// ======================================
+
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+      int count = 0;
+      for(int i = 0 ; i< nums.length ; i++){
+        if(nums[i] == k) count++;
+        int left  = 0 ;
+        left+=nums[i];
+        for(int j = i+1 ; j <nums.length ; j++){
+            left+=nums[j];
+            
+            if(left  == k ){
+                count++;
+                
+            }
+            
+        }
+      }
+      return count;
+    }
+}
